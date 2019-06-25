9954223ac1526fb7421ed77ca4e8f2c3f6efd562
Chris Stephenson <stephensonc@allegheny.edu>
2019-06-25 17:12:40 -0400

    Hopefully the angerykc command works as intended

--- worker.py
+++ worker.py
@@ -2,7 +2,6 @@ import discord
 import asyncio
 import random
 
-
 client = discord.Client()
 
 @client.event
@@ -12,16 +11,31 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+num_jpg = 1
+num_png = 1
+
 @client.event
 async def on_message(message):
     channel = message.channel
     if message.author == client.user:
         return
 
-    if message.content.startswith("!"):
+    if message.content.startswith("!"):#if the text should be parsed for a command
+
+            #King Crimson command
         if message.content.lower() == "!angerykc":
-            rand = random.randint(0, 1)
-            file_to_send = 'KingCrimson' + str(rand) + '.jpg'
-            await channel.send(file=discord.File('./resources/KingCrimson/' + file_to_send))
+            rand = random.randint(0,1)
+            if rand == 0:
+                rand = random.randint(0, num_jpg)
+                file_to_send = 'KingCrimson' + str(rand) + '.jpg'
+                await channel.send(file=discord.File('./resources/KingCrimson/' + file_to_send))
+            if rand == 1:
+                rand = random.randint(0, num_png)
+                file_to_send = 'KingCrimson' + str(rand) + '.png'
+                await channel.send(file=discord.File('./resources/KingCrimsonPNG/' + file_to_send))
+                #End of King Crimson Command
+
+
+
 
 client.run('NTkyODk4NDM0MzQwNzQ5MzEz.XRGYDg.77FbXwZPipf-Q2k_TEcUVz8IPx8')
