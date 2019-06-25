d6646950bc5f948375dedc5aa3efc1f696e0b34e
Chris Stephenson <stephensonc@allegheny.edu>
2019-06-25 16:28:53 -0400

    Uncertain if version update worked

--- worker.py
+++ worker.py
@@ -12,12 +12,13 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    channel = message.channel
     if message.author == client.user:
         return
 
     if message.content.startswith("!"):
         if message.content.lower() == "!angerykc":
-            await client.send_message(message.channel, "ZA WARUDO")
-            #await message.channel.send(file=discord.File('resources/KingCrimson0.jpg'))
+            #await client.send_message(message.channel, "ZA WARUDO")
+            await channel.send(file=discord.File('resources/KingCrimson0.jpg'))
 
 client.run('NTkyODk4NDM0MzQwNzQ5MzEz.XRGYDg.77FbXwZPipf-Q2k_TEcUVz8IPx8')
