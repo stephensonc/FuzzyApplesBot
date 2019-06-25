6ac9cd8adf66f62ab9622e619859cb514bbc2b1d
Chris Stephenson <stephensonc@allegheny.edu>
2019-06-25 16:04:45 -0400

    Forgot to specify the channel for the image

--- worker.py
+++ worker.py
@@ -18,6 +18,6 @@ async def on_message(message):
     if message.content.startswith("!"):
         if message.content.lower() == "!angerykc":
             #await client.send_message(message.channel, "ZA WARUDO")
-            await channel.send(file=discord.File('resources/KingCrimson0.jpg'))
+            await message.channel.send(file=discord.File('resources/KingCrimson0.jpg'))
 
 client.run('NTkyODk4NDM0MzQwNzQ5MzEz.XRGYDg.77FbXwZPipf-Q2k_TEcUVz8IPx8')
