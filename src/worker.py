a7f47b56ea5656170db35c62f1cdcd61577d02e6
Chris Stephenson <stephensonc@allegheny.edu>
2019-06-27 00:56:18 -0400

    Added EraseTime.png to resources in preparation for new commmand

--- src/worker.py
+++ src/worker.py
@@ -9,7 +9,7 @@ client = discord.Client()
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
-    print(client.user.id)
+    #print(client.user.id)
     game = discord.Game("with time")
     await client.change_presence(status = discord.Status.online, activity = game)
     print('------')
