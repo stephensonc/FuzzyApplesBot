a83871dded7fd1c43383b6bcfe052a2eae04d1cb
Christopher Stephenson <stephensonc@allegheny.edu>
2020-06-08 15:20:36 -0400

    Reinstated workaround

--- src/spotify_commands.py
+++ src/spotify_commands.py
@@ -15,12 +15,11 @@ async def playSong(message):
         try:
             audio_source = discord.FFmpegPCMAudio('./resources/mp3s/HeheBoi.mp3')
             voice_client.play(audio_source, after=lambda x: print('Played audio.'))
+            playing_for = 0
+            while(voice_client.is_playing()):
+                playing_for += 1
+                # wait until end of audio
             await voice_client.disconnect()
-            # playing_for = 0
-            # while(voice_client.is_playing()):
-            #     playing_for += 1
-            #     # wait until end of audio
-            # await voice_client.disconnect()
         except:
                 print('Error playing audio file')
                 await voice_client.disconnect()
