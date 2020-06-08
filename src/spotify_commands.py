7f8de7eb6282362da3cb5e6a0879a692b2539bf5
Christopher Stephenson <stephensonc@allegheny.edu>
2020-06-08 13:58:49 -0400

    Add aptfile in attempt to send packets to discord voice

--- src/spotify_commands.py
+++ src/spotify_commands.py
@@ -3,7 +3,7 @@ import asyncio
 
 client = discord.Client()
 if not discord.opus.is_loaded():
-    discord.opus.load_opus()
+    discord.opus.load_opus('opus')
 
 async def playSong(message):
     user=message.author
