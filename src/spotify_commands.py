43805867b2d586bf9706504df1af228640f63d9f
Christopher Stephenson <stephensonc@allegheny.edu>
2020-06-08 15:06:37 -0400

    Create disconnect_from_voice function

--- src/spotify_commands.py
+++ src/spotify_commands.py
@@ -14,7 +14,7 @@ async def playSong(message):
         voice_client = await voice_channel.connect()
         try:
             audio_source = discord.FFmpegPCMAudio('./resources/mp3s/HeheBoi.mp3')
-            voice_client.play(audio_source, after=lambda: await voice_client.disconnect())
+            voice_client.play(audio_source, after=disconnect_from_voice(voice_client))
             # playing_for = 0
             # while(voice_client.is_playing()):
             #     playing_for += 1
@@ -27,3 +27,6 @@ async def playSong(message):
                 raise
     else:
         await message.channel.send('User is not in a voice channel.')
+
+async def disconnect_from_voice(voice_client):
+    await voice_client.disconnect()
