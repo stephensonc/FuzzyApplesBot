06cfc898f152f2163f47cea21266f72ab2091a2a
Christopher Stephenson <stephensonc@allegheny.edu>
2020-06-08 14:58:31 -0400

    place lambda in play call

--- src/spotify_commands.py
+++ src/spotify_commands.py
@@ -12,10 +12,9 @@ async def playSong(message):
     if voice_channel!= None:
         # create StreamPlayer
         voice_client = await voice_channel.connect()
-        disconnect = lambda: await voice_client.disconnect()
         try:
             audio_source = discord.FFmpegPCMAudio('./resources/mp3s/HeheBoi.mp3')
-            voice_client.play(audio_source, after=disconnect())
+            voice_client.play(audio_source, after=lambda: await voice_client.disconnect())
             # playing_for = 0
             # while(voice_client.is_playing()):
             #     playing_for += 1
@@ -23,7 +22,7 @@ async def playSong(message):
             # await voice_client.disconnect()
         except:
                 print('Error playing audio file')
-                # await voice_client.disconnect()
+                await voice_client.disconnect()
                 await message.channel.send('Error playing audio file')
                 raise
     else:
