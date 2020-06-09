26333f40af80ee0a67e09531e3946b9b19e824c8
Christopher Stephenson <stephensonc@allegheny.edu>
2020-06-08 20:20:21 -0400

    Testing printing a user's playlist names

--- src/spotify_commands.py
+++ src/spotify_commands.py
@@ -47,10 +47,7 @@ async def testSpotifyIntegration(message):
     client_credentials_manager = SpotifyClientCredentials()
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     playlists = sp.user_playlists(SPOTIFYUSERNAME)
-    while playlists:
-        for i, playlist in enumerate(playlists['items']):
-            print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
-            if playlists['next']:
-                playlists = sp.next(playlists)
-            else:
-                playlists = None
+    playlistnames = ''
+    for list in playlists:
+        playlistnames += list['name'] + '\n'
+    await message.channel.send(playlistnames)
